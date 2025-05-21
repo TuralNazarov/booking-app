9c3fb91ae45ece552da9fea978ebfd959ca6c6eb
4DMiR4L <153876304+4DMiR4L@users.noreply.github.com>
2025-05-21 20:29:46 +0400

    Add AOP logging for service methods
    Introduce `BookingAop` class to log execution details of service methods. This includes logging method start and end, as well as capturing return values for specific methods in `BookingService` and `FlightService`.

---
+++ src/main/java/project/aop/BookingAop.java
@@ -0,0 +1,38 @@
+package project.aop;
+
+
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class BookingAop {
+
+    @Before("execution(* project.service.*(..))")
+    public void LogBefore(JoinPoint joinPoint) {
+        log.info("method starting: " + joinPoint.getSignature().getName());
+    }
+
+    @After("execution(* project.service.*(..))")
+    public void LogAfter(JoinPoint joinPoint) {
+        log.info("method ending: " + joinPoint.getSignature().getName());
+    }
+
+    @AfterReturning(value = "execution(* project.service.BookingService(..))",returning = "result")
+    public void LogBookingReturning(JoinPoint joinPoint,Object result) {
+        log.info("Booking created: " + joinPoint.getSignature().getName(),result);
+    }
+
+    @AfterReturning(value = "execution(* project.service.FlightService(..))",returning = "result")
+    public void LogFlightReturning(JoinPoint joinPoint,Object result) {
+        log.info("Flight created: " + joinPoint.getSignature().getName(),result);
+    }
+
+}
